1f4c9aecaa9942a757e3f3f4c9ceb4c991efc5d0
Fabrice Toussaint <fabtoussaint@gmail.com>
2017-07-19 22:55:21 -0400

    config setup, basic watson & twitter setup

---
+++ config/tone.js
@@ -0,0 +1,2 @@
+exports.username = 'c1f0ee73-6d56-4844-a1f4-19efb8d82cff';
+exports.password = 'Q6nMHYIIDo3z';
--- index.js
+++ index.js
@@ -1,10 +1,42 @@
-const express = require('express');
-const app = express();
+var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+var Twit = require('twit')
 
-app.get('/', (req, res) => {
-  res.send('Hello asdfWorldd');
-});
+// Credentials
+var twitter = require('./config/twitter')
+var tone = require('./config/tone');
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+var T = new Twit({
+  consumer_key: twitter.consumer_key,
+  consumer_secret: twitter.consumer_secret,
+  access_token: twitter.access_token,
+  access_token_secret: twitter.access_token_secret,
+  timeout_ms: 60*1000,  // optional HTTP request timeout to apply to all requests.
+})
+
+const options = {
+  screen_name: 'NYCTSubway',
+  // screen_name: 'fabricebttest',
+  count: 1
+};
+
+var tone_analyzer = new ToneAnalyzerV3({
+  username: tone.username,
+  password: tone.password,
+  version_date: '2016-05-19'
 });
+let lastTweet;
+
+
+setInterval(() => {
+  T.get('statuses/user_timeline', options, (err, data) => {
+    if (lastTweet !== data[0].text) {
+      lastTweet = data[0].text;
+      tone_analyzer.tone({text: lastTweet}, (err, tone) => {
+              console.log(lastTweet);
+        if(err) {console.log(err)}
+        else {console.log(JSON.stringify(tone, null, 2))}
+      });
+    }
+  });
+
+}, 1000);
